6f7b115b6edd68eff6b4498ad3d4b43d13cfac98
RushikeshNilate <rnilate1999@gmail.com>
2023-02-21 23:42:34 +0530

    Created Successfully

---
+++ FirstJdbc.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+class FirstJdbc{
+public static void main(String[]args){
+try{
+DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+}
+catch(Exception e){
+System.out.println(e);
+}
+}
+}
